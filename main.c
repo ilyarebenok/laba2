4ab6dc662f33f0672aea2ec6cb9904338fd63fb4
ilyarebenok <ilyarebenokk@ya.ru>
2016-06-10 11:29:39 +0300

    Calc sqrt

--- main.c
+++ main.c
@@ -9,6 +9,13 @@ int main()
 	if(a != 0 && b != 0 && c != 0)
 	{
 		float d = b * b - 4 * a * c;
+		if(d > 0)
+			printf("Yravnenie imeet resheniya %f %f \n", (-b + sqrt(d)) /2 *a, (-b - sqrt(d)) / 2 * a);
+		else 
+		{
+		puts("Yravnenie ne imeet resheniya");
+		return 0;
+		}
 	}
 	else 
 		puts("Eto ne kvadratnoe yravnenie"); 
