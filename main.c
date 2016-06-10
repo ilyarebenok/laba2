86d6c1531e0d4173f3f0ece405b547fcec5b9c9c
ilyarebenok <ilyarebenokk@ya.ru>
2016-06-10 11:17:59 +0300

    Calculation discrimenant

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
@@ -7,7 +8,7 @@ int main()
 	scanf("%f %f %f", &a, &b, &c");
 	if(a != 0 && b != 0 && c != 0)
 	{
-	
+		float d = b * b - 4 * a * c;
 	}
 	else 
 		puts("Eto ne kvadratnoe yravnenie"); 
