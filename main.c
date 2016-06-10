6b492b94812a24ddd9b15c0547c4ca048abedea6
ilyarebenok <ilyarebenokk@ya.ru>
2016-06-10 11:09:28 +0300

    Check is square aquation

--- main.c
+++ main.c
@@ -5,6 +5,12 @@ int main()
 	float a,b,c;
 	puts("Vvedite a,b,c");
 	scanf("%f %f %f", &a, &b, &c");
+	if(a != 0 && b != 0 && c != 0)
+	{
+	
+	}
+	else 
+		puts("Eto ne kvadratnoe yravnenie"); 
 	return 0;
 }
 
